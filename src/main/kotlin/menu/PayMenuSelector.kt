e3f147c3bd1b8f4a5ca38f6554105dde15f4066d
hongbeomi <dks3963@naver.com>
2021-10-26 18:39:35 +0900

    MenuSelector remove

--- src/main/kotlin/menu/PayMenuSelector.kt
+++ src/main/kotlin/menu/PayMenuSelector.kt
@@ -5,4 +5,29 @@ import printer.PayMenuPrinter
 
 class PayMenuSelector(
     val printer: MenuPrinter = PayMenuPrinter()
-) : MenuSelector(printer)
+) {
+
+    fun select(): MutableList<Menu> {
+        val menuList = mutableListOf<Menu>()
+
+        while (true) {
+            printer.printMenu()
+            val ordinal = readLine()?.toIntOrNull()
+
+            when {
+                ordinal == 5 -> break
+                ordinal == null || (ordinal - 1) !in 0 until Menu.values().size -> {
+                    printer.error()
+                    continue
+                }
+                else -> {
+                    val menu = Menu.values()[ordinal - 1]
+                    printer.print(menu.nameKR)
+                    menuList.add(menu)
+                }
+            }
+        }
+        return menuList
+    }
+
+}
