244959e123ab2140d62add7b6b6ccc7d70ddda2e
hongbeomi <dks3963@naver.com>
2021-11-15 20:06:05 +0900

    using sumBu

--- src/main/kotlin/calculator/PriceCalculator.kt
+++ src/main/kotlin/calculator/PriceCalculator.kt
@@ -4,13 +4,6 @@ import menu.Menu
 
 class PriceCalculator {
 
-    fun calculate(menuList: List<Menu>): Int {
-        var sumPrice = 0
-
-        menuList.forEach {
-            sumPrice += it.price
-        }
-        return sumPrice
-    }
+    fun calculate(menuList: List<Menu>) = menuList.sumBy { it.price }
 
 }
